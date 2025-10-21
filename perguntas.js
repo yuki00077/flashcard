4c8645bf782c409edd8d93efdb0c09ada4939956
yuki <luana.klabunde@escola.pr.gov.br>
2025-10-21 00:09:35 +0000

    11

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica2',
+    'o que é HTML?',
+    'é o código padrão usado para criar a estrutura e o conteúdo de páginas da web',
+)
+
+criaCartao(
+    'Geografia',
+    'qua?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
