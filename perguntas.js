3a9347eddfbd3625c19b001305bbdd3cd9e8792f
yuki <luana.klabunde@escola.pr.gov.br>
2025-10-22 01:00:12 +0000

    2

--- perguntas.js
+++ perguntas.js
@@ -19,20 +19,20 @@ criaCartao(
 criaCartao(
     'Língua portuguêsa',
     'quem foi chico buarque?',
-    ''
+    'Chico Buarque é um influente cantor, compositor, dramaturgo, escritor e ator brasileiro, nascido no Rio de Janeiro em 19 de junho de 1944. Conhecido como um dos maiores nomes da Música Popular Brasileira (MPB)'
 )
 
 
 criaCartao(
-    'Geografia',
-    'Qual é o maior continente do mundo?',
-    'O maior continetente do mundo é a Ásia'
+    'Historia',
+    'Em que ano ocorreu a Revolução Francesa?',
+    'A Revolução Francesa ocorreu no ano de 1789, começando com a Queda da Bastilha em 14 de julho e terminando em 1799. '
 )
 
 
 criaCartao(
-    'Geografia',
-    'Qual é o maior continente do mundo?',
+    'Lingua Inglesa',
+    'como e fala ?',
     'O maior continetente do mundo é a Ásia'
 )
 
