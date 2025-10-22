dff1273190e9e03e1d84c0fdda8a22cb077643aa
yuki <luana.klabunde@escola.pr.gov.br>
2025-10-22 00:49:12 +0000

    7777

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,94 @@
 criaCartao(
-    'matematica2',
-    'o que é HTML?',
-    'é o código padrão usado para criar a estrutura e o conteúdo de páginas da web',
+    'Matematica2',
+    'O que é HTML?',
+    'É o código padrão usado para criar a estrutura e o conteúdo de páginas da web',
 )
 
 criaCartao(
     'Geografia',
-    'qua?',
-    'A capital da França é Paris'
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matematica',
+    'O que é bhaskara?',
+    ' Bhaskara é um método matemático, ou fórmula, usado para encontrar as raízes de equações do segundo grau (\(ax^{2}+bx+c=0\)). '
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Língua portuguêsa',
+    'quem foi chico buarque?',
+    ''
 )
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continetente do mundo é a Ásia'
+)
+
